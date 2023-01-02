ea7cf6a7327319c048a1a8537c61e3ef80335cc7
dhkham <khamdong@gmail.com>
2023-01-02 21:30:15 +0900

    230102_4

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,12 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 14:02:40 by dkham             #+#    #+#             */
-/*   Updated: 2023/01/02 20:30:39 by dkham            ###   ########.fr       */
+/*   Updated: 2023/01/02 21:29:53 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
+// hi
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
@@ -96,6 +96,11 @@ int	ft_parse_type(t_flags *flags, const char **format, va_list ap)
 		count += print_c(flags, va_arg(ap, int));
 	else if (**format == 's')
 		count += print_s(flags, va_arg(ap, char *));
+<<<<<<< Updated upstream
+=======
+		//printf("")
+	}
+>>>>>>> Stashed changes
 	// else if (**format == 'p')
     //     count += print_p(flags, va_arg(ap, unsigned long long));
 	else if (**format == 'i' || **format == 'd')
