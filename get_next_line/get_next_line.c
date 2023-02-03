cd53aaf277bab2f4b45edf8d94c287dfd0186d11
dhkham <khamdong@gmail.com>
2023-02-03 13:31:41 +0900

    230203

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 20:27:15 by dkham             #+#    #+#             */
-/*   Updated: 2023/02/02 21:23:45 by dkham            ###   ########.fr       */
+/*   Updated: 2023/02/03 12:25:02 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,36 +21,39 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	cur = find_fd_node(fd, &head); // linked list로 각 fd별로 노드에 저장
+	cur = find_fd_node(fd, &head); // 해당 fd의 노드 가리키는 포인터 return, 없으면 새로운 노드 생성
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1)); // 파일에서 nbyte씩 읽어와 넣을 버퍼 만들기
 	if (buf == NULL)
-		return (remove_cur_ptr(cur, &head)); // 동적할당 실패 시 노드 다 터뜨리기
+		return (remove_cur_ptr(cur, &head));
 	line = read_line(cur, head, buf);
-	free(buf);
+	free(buf); // free(cur)?? free(cur->data)??
 	return (line);
 }
 
 t_list	*find_fd_node(int fd, t_list **head)
 {
 	t_list	*cur;
+	t_list	*prior;
 
 	cur = *head;
+	prior = NULL;
 	while (cur) // head가 빈 linked list가 아닐 때 loop
 	{
 		if (cur->fd == fd) // fd가 이미 저장되어 있으면 해당 fd의 linked list 노드를 가리키는 포인터 return
 			return (cur);
+		prior = cur;
 		cur = cur->next;
 	}
 	if (cur == NULL) // head가 빈 linked list일 경우 새로운 linked list 노드 생성
 	{
 		cur = (t_list *)malloc(sizeof(t_list));
 		if (cur == NULL)
-			return (remove_cur_ptr(cur, head)); // 동적할당 실패 시 노드 다 터뜨리기
+			return (NULL);
 		*head = cur; // head 포인터가 새로 생성된 linked list를 가리키게 함
 		cur->fd = fd;
 		cur->data = NULL;
 		cur->next = NULL;
-		cur->prev = NULL;
+		cur->prev = prior;
 	}
 	return (cur);
 }
@@ -62,18 +65,18 @@ char	*read_line(t_list *cur, t_list *head, char *buf)
 	while (1)
 	{
 		read_size = read(cur->fd, buf, BUFFER_SIZE); //fd에서 nbyte만큼 읽어와 buf에 저장
-		if (read_size == -1) //1. read error
-			return (remove_cur_ptr(cur, &head)); //실패 시 노드 다 터뜨리기
-		if (read_size == 0)  //2. EOF
+		if (read_size == -1)   //1. read error
+			return (remove_cur_ptr(cur, &head)); // 질문: cur->data까지 free 하는게 맞나?
+		if (read_size == 0)    //2. EOF
 		{
 			if (cur->data == NULL) // 아예 빈 파일일 경우
-				return (remove_cur_ptr(cur, &head));
+				return (remove_cur_ptr(cur, &head)); // 질문: cur->data까지 free 하는게 맞나?
 			return (make_line(cur, &head));
 		}
-		buf[read_size] = '\0'; // 3. 정상적인 read 실행 시
+		buf[read_size] = '\0'; //3. 정상적인 read 실행 시
 		cur->data = ft_strjoin(cur->data, buf); // data에 buf를 붙여 cur->data에 저장
 		if (cur->data == NULL)
-			return (remove_cur_ptr(cur, &head)); // 실패 시 노드 다 터뜨리기
+			return (remove_cur_ptr(cur, &head)); // 질문: cur->data까지 free 하는게 맞나?
 		if (ft_strchr(cur->data, '\n'))
 			return (make_line(cur, &head));
 	}
@@ -108,7 +111,7 @@ char	*make_line(t_list *cur, t_list **head)
 
 void	*remove_cur_ptr(t_list *cur, t_list **head)
 {
-	if (cur == NULL || head == NULL) // 둘 중 하나라도 NULL이면 NULL 리턴
+	if (cur == NULL || *head == NULL) // 둘 중 하나라도 NULL이면 NULL 리턴
 		return (NULL);
 	if (cur->prev != NULL) 	// 현 노드가 첫 노드가 아니면 이전 노드의 next가 현 노드의 next를 가리키게 함
 		cur->prev->next = cur->next;
