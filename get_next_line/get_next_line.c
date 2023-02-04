495874f30125ad61216392768bfb857b2221fecc
dhkham <khamdong@gmail.com>
2023-02-04 16:22:03 +0900

    no comments

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 20:27:15 by dkham             #+#    #+#             */
-/*   Updated: 2023/02/04 14:58:26 by dkham            ###   ########.fr       */
+/*   Updated: 2023/02/04 15:57:34 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,14 +39,12 @@ t_list	*find_fd_node(int fd, t_list **head)
 
 	cur = *head;
 	prior = NULL;
-	while (cur) // head가 빈 linked list가 아닐 때 loop
+	while (cur && cur->fd != fd)
 	{
-		if (cur->fd == fd) // fd가 이미 저장되어 있으면 해당 fd의 linked list 노드를 가리키는 포인터 return
-			return (cur);
 		prior = cur;
 		cur = cur->next;
 	}
-	if (cur == NULL) // 해당 fd 노드 없을 경우 새 노드 생성
+	if (cur == NULL)
 	{
 		cur = (t_list *)malloc(sizeof(t_list));
 		if (cur == NULL)
@@ -70,18 +68,18 @@ char	*read_line(t_list *cur, t_list **head, char *buf)
 
 	while (1)
 	{
-		read_size = read(cur->fd, buf, BUFFER_SIZE); //fd에서 nbyte만큼 읽어와 buf에 저장
-		if (read_size == -1)   //1. read error
+		read_size = read(cur->fd, buf, BUFFER_SIZE);
+		if (read_size == -1)
 			return (remove_cur_ptr(cur, head));
-		if (read_size == 0)    //2. EOF
+		if (read_size == 0)
 		{
-			if (cur->data == NULL || !*(cur->data)) // 아예 빈 파일일 경우
+			if (cur->data == NULL || !*(cur->data))
 				return (remove_cur_ptr(cur, head));
 			return (make_line(cur, head));
 		}
-		buf[read_size] = '\0'; //3. 정상적인 read 실행 시
+		buf[read_size] = '\0';
 		tmp = cur->data;
-		cur->data = ft_strjoin(cur->data, buf); // data에 buf를 붙여 cur->data에 저장
+		cur->data = ft_strjoin(cur->data, buf);
 		free(tmp);
 		if (cur->data == NULL)
 			return (remove_cur_ptr(cur, head));
@@ -95,8 +93,8 @@ char	*make_line(t_list *cur, t_list **head)
 	char	*line;
 	char	*lb;
 
-	lb = ft_strchr(cur->data, '\n'); // '\n'이 있는 위치를 가리키는 포인터
-	if (lb == NULL) // 파일에 '\n'이 없는 경우 (예. hello(EOF)))
+	lb = ft_strchr(cur->data, '\n');
+	if (lb == NULL)
 	{
 		line = ft_strdup(cur->data);
 		if (line == NULL)
@@ -117,16 +115,16 @@ char	*make_line(t_list *cur, t_list **head)
 
 void	*remove_cur_ptr(t_list *cur, t_list **head)
 {
-	if (cur == NULL || *head == NULL) // 둘 중 하나라도 NULL이면 NULL 리턴
+	if (cur == NULL || *head == NULL)
 		return (NULL);
-	if (cur->prev != NULL) 	// 현 노드가 첫 노드가 아니면 이전 노드의 next가 현 노드의 next를 가리키게 함
+	if (cur->prev != NULL)
 		cur->prev->next = cur->next;
-	if (cur->next != NULL) // 현 노드가 마지막 노드가 아니면 다음 노드의 prev가 현 노드의 prev를 가리키게 함
+	if (cur->next != NULL)
 		cur->next->prev = cur->prev;
-	if (cur == *head) // 현 노드가 첫 노드면(혹은 노드가 하나면)
+	if (cur == *head)
 		*head = cur->next;
 	free(cur->data);
-	cur->data = NULL; // free 후 NULL로 초기화
+	cur->data = NULL;
 	free(cur);
 	return (NULL);
 }
